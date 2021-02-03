d3b9ab020f02591598f314f52d5b5ea96c747484
Jeff Mitchell <jeff@foschimitchell.ca>
2021-02-02 19:47:34 -0800

    20210202 Update

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// supporting javascript code for thejeffism.ca v2
+//
+// header
+'use strict'
+console.log('Here\'s a hidden message');
+
+// display date
+let date = new Date();
+document.getElementById("date").innerHTML = date.toLocaleDateString();
