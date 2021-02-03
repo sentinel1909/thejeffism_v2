844943afd628e11d58e47ca390cbe80d4b4ce623
Jeff Mitchell <jeff@foschimitchell.ca>
2021-02-02 22:19:34 -0800

    Deleted app.js

--- app/app.js
+++
@@ -1,9 +0,0 @@
-// supporting javascript code for thejeffism.ca v2
-//
-// header
-'use strict'
-console.log('Here\'s a hidden message');
-
-// display date
-let date = new Date();
-document.getElementById("date").innerHTML = date.toLocaleDateString();
